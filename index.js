17cfc078fb019d1c1258412b1d37c57ff3ea4adb
HuyCongJr <80660191+HuyCongJr@users.noreply.github.com>
2022-03-05 14:16:08 +0700

    Add files via upload
    add code

---
+++ index.js
@@ -0,0 +1,16 @@
+function toggleMobileMenu(menu) {
+  menu.classList.toggle("open");
+}
+
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("hamburger-icon").style.top = "0";
+//   } else {
+//     document.getElementById("hamburger-icon").style.top = "-50px";
+//   }
+// }
+// scrollFunction();
